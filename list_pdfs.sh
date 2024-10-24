fbf4943c0cc820b81f7a73392d57b2d14a748f2e
Benjamin <betheridge@turing.ac.uk>
2024-10-24 23:00:46 +0100

    prettify

--- list_pdfs.sh
+++ list_pdfs.sh
@@ -7,10 +7,18 @@ OUTPUT_FILE="content/talks_pdfs.json"
 # Start the JSON array
 echo "[" > $OUTPUT_FILE
 
+replace_underscores_and_dashes() {
+    echo "$1" | sed 's/[_-]/ /g'
+}
+
+capitalize_first_letter() {
+    echo "$1" | awk '{ for(i=1; i<=NF; i++) { $i = toupper(substr($i, 1, 1)) substr($i, 2) } }1'
+}
+
 # Loop through each PDF and add it to the JSON file
 for pdf in $TALKS_DIR/*.pdf; do
     filename=$(basename -- "$pdf")
-    title="${filename%.*}" # Remove extension for the title
+    title=$(capitalize_first_letter "$(replace_underscores_and_dashes "${filename%.*}")") # Remove extension and prettify title
     date=$(git log --follow --format=%aI -- "$pdf" | tail -1 | cut -d'T' -f1)
     echo "  { \"file\": \"$filename\", \"title\": \"$title\", \"date\": \"$date\" }," >> $OUTPUT_FILE
 done
@@ -18,4 +26,3 @@ done
 # Remove the trailing comma from the last entry and close the array
 sed -i '' '$ s/,$//' $OUTPUT_FILE
 echo "]" >> $OUTPUT_FILE
-
