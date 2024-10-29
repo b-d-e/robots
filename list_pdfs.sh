39cae2e983b1f73563b2c4145a1c611b5869f02f
Benjamin <betheridge@turing.ac.uk>
2024-10-29 15:04:02 +0000

    get git log from git context of sub repo

--- list_pdfs.sh
+++ list_pdfs.sh
@@ -15,16 +15,22 @@ capitalize_first_letter() {
     echo "$1" | awk '{ for(i=1; i<=NF; i++) { $i = toupper(substr($i, 1, 1)) substr($i, 2) } }1'
 }
 
+# Change directory to TALKS_DIR to get the correct Git context
+cd "$TALKS_DIR" || exit 1
+
 # Loop through each PDF and add it to the JSON file
-for pdf in "$TALKS_DIR"/*.pdf; do
+for pdf in ./*.pdf; do
     filename=$(basename -- "$pdf")
     title=$(capitalize_first_letter "$(replace_underscores_and_dashes "${filename%.*}")") # Remove extension and prettify title
 
-    # Get the creation date of the file in the repo
+    # Get the creation date of the file in the submodule repo
     date=$(git log --diff-filter=A --format=%aI -- "$pdf" | tail -1 | cut -d'T' -f1)
 
-    echo "  { \"file\": \"$filename\", \"title\": \"$title\", \"date\": \"$date\" }," >> "$OUTPUT_FILE"
+    echo "  { \"file\": \"$filename\", \"title\": \"$title\", \"date\": \"$date\" }," >> "../$OUTPUT_FILE"
 done
+
+# Go back to the original directory
+cd - || exit 1
 
 # Remove the trailing comma from the last entry and close the array
 if [ "$OSTYPE" = "darwin"* ]; then
