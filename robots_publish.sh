b772e61ee125d8cd6e8cd84a1ab6a36b8e42bab9
Benjamin <betheridge@turing.ac.uk>
2024-11-24 16:19:46 +0000

    try git clone priv

--- robots_publish.sh
+++ robots_publish.sh
@@ -1,4 +1,5 @@
 git clone git@github.com:b-d-e/talks.git static/talks/
 sh list_pdfs.sh
 zola build --force --base-url https://robots.ox.ac.uk/~be --output-dir ../WWW
+git clone git@github.com:b-d-e/robots_private.git ../WWW/private
 chmod -R 777 ../WWW
